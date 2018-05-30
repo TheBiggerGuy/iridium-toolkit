8dfa4d70a51b2e54de2e85bc6d699f6b5beacb2a
Guy Taylor <thebigguy.co.uk@gmail.com>
2018-05-30 13:04:16 +0100

    Try to get OSX tested via Travis

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,17 +1,41 @@
 #!/bin/bash
 
+set -e
+set -x
+
+# See https://github.com/pyca/cryptography/blob/master/.travis/install.sh
+
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+    sw_vers
 
-    # Install custom requirements on OS X
-    brew install pyenv-virtualenv
+    # install pyenv
+    git clone --depth 1 https://github.com/pyenv/pyenv ~/.pyenv
+    PYENV_ROOT="$HOME/.pyenv"
+    PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init -)"
 
+    PYTHON_VER_LONG='error'
+    PYTHON_VER_SHORT='error'
     case "${TOXENV}" in
-        py27)
-            # Install some custom Python 3.2 requirements on OS X
-            echo 'Nothing to do'
+        py36)
+            PYTHON_VER_LONG='3.6.5'
+            PYTHON_VER_SHORT='3.6'
+            ;;
+        py37)
+            PYTHON_VER_LONG='3.7.15'
+            PYTHON_VER_SHORT='3.7'
             ;;
     esac
+    echo "Setting up Python ${PYTHON_VER_LONG}"
+    pyenv install "${PYTHON_VER_LONG}"
+    pyenv global "${PYTHON_VER_LONG}"
+    pyenv rehash
+
+    eval "python${PYTHON_VER_SHORT} -m venv ~/.travis-venv"
+    source ~/.travis-venv/bin/activate
+
+    pip3 install tox-pyenv
 else
     # Install custom requirements on Linux
-    pip install tox-travis
+    pip3 install tox-travis
 fi
